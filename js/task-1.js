d1b20db2eb3ac829eb377987d1583d57842f8e48
Alexey-Kapitonov <dekaliostro@gmail.com>
2021-02-11 15:11:46 +0200

    hw-7-structure

---
+++ js/task-1.js
@@ -0,0 +1,16 @@
+//=-==-=-=-=--=-=-=-=-=-=-=-=-=-===-
+console.log(" ");
+console.log("Task-1");
+console.log(" ");
+//=-=-=-==-=-=-=-=-=-=-=-==-=-=-=-=-
+
+/*
+
+*/
+
+//Task START
+alert('hello');
+//Task END
+
+console.log(" ");
+console.log("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
