9c37b5d2bd6eb388c2e34419c38f63c3672398bf
Alexey-Kapitonov <dekaliostro@gmail.com>
2021-03-14 21:55:16 +0200

    task-4

--- js/task-4.js
+++ js/task-4.js
@@ -6,11 +6,32 @@ console.log(" ");
 
 /*
 
+Счетчик состоит из спана и кнопок, которые должны увеличивать и уменьшать значение счетчика на 1.
+
+Создай переменную counterValue в которой будет хранится текущее значение счетчика.
+Создай функции increment и decrement для увеличения и уменьшения значения счетчика
+Добавь слушатели кликов на кнопки, вызовы функций и обновление интерфейса
+
 */
 
 // Task START
 
+let value = 0;
 
+const counterRef = document.querySelector('span');
+const decrementRef = document.querySelector('[data-action="decrement"]');
+const incrementRef = document.querySelector('[data-action="increment"]');
+
+const numberUp = () => {
+   counterRef.textContent = value += 1;
+}
+
+const numberDown = () => {
+    counterRef.textContent = value -= 1;
+}
+
+decrementRef.addEventListener('click', numberDown);
+incrementRef.addEventListener('click', numberUp);
 
 //Task END
 
